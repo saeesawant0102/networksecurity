91c9195bfa83ac937c45a5e8e6d66540ae66fcc5
saee <saeesawant0102@gmail.com>
2026-06-09 19:52:36 +0530

    Data Transformation Completed

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -11,7 +11,6 @@ from networksecurity.utils.main_utils.utils import read_yaml_file,write_yaml_fil
 class DataValidation:
     def __init__(self,data_ingestion_artifact:DataIngestionArtifact,
                  data_validation_config:DataValidationConfig):
-        
         try:
             self.data_ingestion_artifact=data_ingestion_artifact
             self.data_validation_config=data_validation_config
